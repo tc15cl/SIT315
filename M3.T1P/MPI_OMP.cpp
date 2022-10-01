2afd96eda0ec8bdce082fb6b5820293c738f6da2
tc15cl <37233314+tc15cl@users.noreply.github.com>
2022-10-01 18:56:09 +1000

    Update MPI_OMP.cpp

--- M3.T1P/MPI_OMP.cpp
+++ M3.T1P/MPI_OMP.cpp
@@ -90,8 +90,8 @@ int main(int argc, char *argv[]) {
 
     //print_matrix(m3); //print matrix output if desired
 
-    //Code to print runtime in ms
-    double runTime= double(endClock-startClock) * 1000;
+    //Code to print runtime in us
+    double runTime= double(endClock-startClock) * 1000 * 1000;
     cout << "\nRuntime: " << runTime << "ms\n";
   }
 
